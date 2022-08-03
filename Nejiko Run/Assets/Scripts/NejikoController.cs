e038c8fe0038f199d16ced1207c172276007f222
yakan777 <yume_drop@hotmail.com>
2022-08-03 14:17:18 +0900

    commit

--- Nejiko Run/Assets/Scripts/NejikoController.cs
+++ Nejiko Run/Assets/Scripts/NejikoController.cs
@@ -7,17 +7,30 @@ public class NejikoController : MonoBehaviour
     const int MinLane = -2;
     const int MaxLane = 2;
     const float LaneWidth = 1.0f;
+    const int DefaultLife=3;
+    const float StunDuration=0.5f;
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection=Vector3.zero;
     int targetLane;
+    int life = DefaultLife;
+    float recoverTime=0.0f;
 
     public float gravity;
     public float speedZ;
     public float speedX;
     public float speedJump;
     public float accelerationZ;
+
+    public int Life()
+    {
+        return life;
+    }
+    bool IsStun()
+    {
+        return recoverTime>0.0f || life<=0;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -33,11 +46,18 @@ public class NejikoController : MonoBehaviour
         if(Input.GetKeyDown("right")) MoveToRight();
         if(Input.GetKeyDown("space")) Jump();
 
-        float acceleratedZ = moveDirection.z+(accelerationZ*Time.deltaTime);
-        moveDirection.z=Mathf.Clamp(acceleratedZ,0,speedZ);
+        if(IsStun())
+        {
+            moveDirection.x=0.0f;
+            moveDirection.z=0.0f;
+            recoverTime-=Time.deltaTime;
+        }else{
+            float acceleratedZ = moveDirection.z+(accelerationZ*Time.deltaTime);
+            moveDirection.z=Mathf.Clamp(acceleratedZ,0,speedZ);
 
-        float ratioX = (targetLane*LaneWidth-transform.position.x)/LaneWidth;
-        moveDirection.x=ratioX*speedX;
+            float ratioX = (targetLane*LaneWidth-transform.position.x)/LaneWidth;
+            moveDirection.x=ratioX*speedX;
+        }
 
         moveDirection.y-=gravity*Time.deltaTime;
 
@@ -52,19 +72,36 @@ public class NejikoController : MonoBehaviour
 
     public void MoveToLeft()
     {
+        if(IsStun()) return;
         if(controller.isGrounded && targetLane > MinLane) targetLane--;
     }
     public void MoveToRight()
     {
-        if(controller.isGrounded && targetLane > MinLane) targetLane++;
+        if(IsStun()) return;
+        if(controller.isGrounded && targetLane < MaxLane) targetLane++;
     }
     public void Jump()
     {
+        if(IsStun()) return;
         if(controller.isGrounded)
         {
             moveDirection.y=speedJump;
 
             animator.SetTrigger("jump");
+        }
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(IsStun()) return;
+
+        if(hit.gameObject.tag=="Robo")
+        {
+            life--;
+            recoverTime=StunDuration;
+
+            animator.SetTrigger("damage");
+
+            Destroy(hit.gameObject);
         }
     }
 
